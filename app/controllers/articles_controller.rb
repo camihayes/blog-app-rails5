0e18ad5cb0b0079e4a72345ae2ffe136978b823b
camihayes <camihayes72@gmail.com>
2017-01-25 14:13:43 +0000

    complete article feature

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,31 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article has been created"
+      redirect_to articles_path
+    else
+      flash.now[:danger] = "Article has not been created"
+      render :new
+    end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end 
+  
+  private
+  
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+end
+
